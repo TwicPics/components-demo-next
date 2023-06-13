b4364141577bb3016090471199614312dbdbd0ef
Miguel Beignon <miguel@twic.pics>
2023-06-13 15:43:55 +0200

    Components version 0.19.0

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,16 @@
-import { installTwicPics } from "@twicpics/components/react";
+import { TwicInstall } from "@twicpics/components/react";
 import "@twicpics/components/style.css";
 import "../styles/globals.scss";
 import Head from "next/head";
 
-installTwicPics({
-  domain: `https://demo.twic.pics`,
-  anticipation: 0.5,
-  step: 100,
-});
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <TwicInstall
+        domain="https://demo.twic.pics"
+        anticipation="0.5"
+        step="100"
+      />
       <Head>
         <title>TwicPics x Next</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
